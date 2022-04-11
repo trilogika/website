49eda9afb10073886ec4444af8355ee18dcde351
thiagomajesk <thiagomgoulart@gmail.com>
2022-04-11 11:51:37 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Start project
+
+```
+npm run dev
+```
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["index.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      }
+    }
+  },
+  plugins: [],
+}
