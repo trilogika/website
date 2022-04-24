d3def1fdccd657af01a182504d78100410ddd8d7
thiagomajesk <thiagomgoulart@gmail.com>
2022-04-24 12:56:02 -0300

    update brand color and logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,20 @@ module.exports = {
   content: ["index.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {
+      "colors": {
+        "brand": {
+          "50": "#E6ECFF",
+          "100": "#CCDAFF",
+          "200": "#99B4FF",
+          "300": "#678FFE",
+          "400": "#346AFE",
+          "500": "#0247FE",
+          "600": "#0137CB",
+          "700": "#012998",
+          "800": "#001B66",
+          "900": "#000E33"
+        }
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
       }
